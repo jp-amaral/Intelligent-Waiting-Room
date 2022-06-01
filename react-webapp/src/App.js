871ac9efe477490c53251f8208c58b27bb102d0b
João Viegas <joao.antonio.viegas@ua.pt>
2022-06-01 23:43:06 +0100

    webapp-uprade

--- react-webapp/src/App.js
+++ react-webapp/src/App.js
@@ -1,25 +1,11 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import PulseMeasurement from "./pages/PulseMeasurement";
-import About from "./pages/About";
-
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/PulseMeasurement" exact component={PulseMeasurement} />
-          <Route path="/about" exact component={About} />
-
-        </Switch>
-        
-      </Router>
+      <Home/>
     </div>
   );
 }
--- react-webapp/src/pages/Home.js
+++ react-webapp/src/pages/Home.js
@@ -7,39 +7,14 @@ import { Image,  StyleSheet, Text, View } from 'react-native';
 import Box from '@material-ui/core/Box';
 import { AllInbox, CenterFocusStrong } from "@material-ui/icons";
 
-const Contador = () => {
-  
-  return (
-    <Box
-      style={{
-        backgroundColor: '#333',
-      
-        borderRadius: 10,
-        color: '#eee',
-        display: "flex",
-        float:'right',
-        minHeight: 200,
-        padding: 12,
-        marginRight: 130,
-        marginTop:50,
-        width: 300,
-   
-      }}
-    >
-      Contagem de pessoas
-    </Box>
-  );
-}
 
 function Home() {
   return (
-    <div className="linha" style={{backgroundcolor: "black"}}>
-      
-       <div className="coluna-50" >
-        <Image source={BannerImage} style={{ width: 1300, height: 755, borderRadius: 10}}/>
-      </div>      
-      <div className="coluna-60" ><Contador></Contador></div>
-     
+    <div className="linha">
+      <img className="room" src={BannerImage} />
+      <div className="rect">
+        <h1></h1>
+      </div>
     </div>
 
     
