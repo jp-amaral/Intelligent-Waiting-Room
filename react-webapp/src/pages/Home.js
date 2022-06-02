810865c1afa2f92c11e208edd8d28e1e58e49848
João Viegas <joao.antonio.viegas@ua.pt>
2022-06-02 01:30:27 +0100

    webapp-update

--- react-webapp/src/pages/Home.js
+++ react-webapp/src/pages/Home.js
@@ -13,7 +13,12 @@ function Home() {
     <div className="linha">
       <img className="room" src={BannerImage} />
       <div className="rect">
-        <h1></h1>
+        <h1 className="npd">Nº of people detected</h1>
+      </div>
+      <div className="rect2">
+        <h1 className="pdr">People detected in the room</h1>
+      </div>
+      <div className="rect3">
       </div>
     </div>
 
