8f7d328aeb5e263684936afb37474b88601e9794
João Viegas <joao.antonio.viegas@ua.pt>
2022-06-05 12:53:14 +0100

    frontend app ended

--- react-webapp/src/pages/Home.js
+++ react-webapp/src/pages/Home.js
@@ -1,24 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
 import BannerImage from "../assets/room.jpeg";
 import "../styles/Home.css";
-import { Image,  StyleSheet, Text, View } from 'react-native';
-
-import Box from '@material-ui/core/Box';
-import { AllInbox, CenterFocusStrong } from "@material-ui/icons";
-
+import {Icon} from '@iconify/react';
 
 function Home() {
+  //atribuir valores random a "npessoas"
+  const [npessoas, setNpessoas] = React.useState(0);
+  
+  useEffect(() => {
+    setInterval(() => {
+      setNpessoas(Math.floor(Math.random() * (40)));
+    }, 1000);
+  }, []);
+
+
+
   return (
     <div className="linha">
       <img className="room" src={BannerImage} />
       <div className="rect">
-        <h1 className="npd">Nº of people detected</h1>
+        <h1 className="npd">{npessoas}</h1>
+        <Icon className="iconpessoas" icon="fluent:people-24-filled"   />
       </div>
       <div className="rect2">
-        <h1 className="pdr">People detected in the room</h1>
+        <h1 className="pdr">People counter</h1>
       </div>
       <div className="rect3">
+      </div>
+      <div className='info' >
+        <Icon icon="entypo:info-with-circle" color="#699bf7" height="100" />
       </div>
     </div>
 
